13b5c5f628d497c664f310a2ce26637e039c7678
jeanpaulwood <jeanpaulwood08@gmail.com>
2021-02-23 07:11:18 -0300

    V 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        fontFamily: 'Newsreader',
+      ),
     );
   }
 }
@@ -22,14 +25,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
+  final List<Transaction> _transactions = [];
+  /*= [
     Transaction(
         id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now()),
     Transaction(
         id: 't3', title: 'Conta de água', value: 211.30, date: DateTime.now()),
     Transaction(
         id: 't4', title: 'Conta de gás', value: 211.30, date: DateTime.now()),
-  ];
+  ];*/
 
   void _addTransaction(String title, double value) {
     Transaction newTransaction = Transaction(
@@ -58,7 +62,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: Text(
+          'Despesas Pessoais',
+        ),
         actions: [
           IconButton(
               icon: Icon(Icons.add),
